c15a8b8e9ef05920a3bc17d625d449d9d1dc08ad
pnzenang <pnzenang@gmail.com>
2025-07-29 00:41:37 -0400

    7.29.commit

--- components/navbar/LinksDropdown.tsx
+++ components/navbar/LinksDropdown.tsx
@@ -10,26 +10,53 @@ import { FaAlignLeft } from 'react-icons/fa6';
 import { LuCircleUser } from 'react-icons/lu';
 import { Button } from '../ui/button';
 import { links } from '@/utils/links';
+import { SignedIn, SignedOut, SignInButton, SignUpButton } from '@clerk/nextjs';
+import SignOutLink from './SignOutLink';
+import UserIcon from './UserIcon';
 
 const LinksDropdown = () => {
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <Button variant='outline' className='flex gap-5 max-w-[100px]'>
-          <FaAlignLeft className='w-6 h-6' />
-          <LuCircleUser className='w-6 h-6' />
+        <Button variant='outline' className='flex gap-4 max-w-[100px]'>
+          <SignedOut>
+            <FaAlignLeft className='w-10 h-10 ' />
+          </SignedOut>
+          <SignedIn>
+            <FaAlignLeft className='w-10 h-10 text-primary' />
+          </SignedIn>
+          <UserIcon />
         </Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent className='w-40 ' align='start' sideOffset={10}>
-        {links.map((link) => {
-          return (
-            <DropdownMenuItem key={link.href}>
-              <Link href={link.href} className='capitalize w-full'>
-                {link.label}
-              </Link>
-            </DropdownMenuItem>
-          );
-        })}
+        <SignedOut>
+          <DropdownMenuItem>
+            <SignInButton mode='modal'>
+              <button className='w-full text-left'>Login</button>
+            </SignInButton>
+          </DropdownMenuItem>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem>
+            <SignUpButton mode='modal'>
+              <button className='w-full text-left'>Register</button>
+            </SignUpButton>
+          </DropdownMenuItem>
+        </SignedOut>
+        <SignedIn>
+          {links.map((link) => {
+            return (
+              <DropdownMenuItem key={link.href}>
+                <Link href={link.href} className='capitalize w-full'>
+                  {link.label}
+                </Link>
+              </DropdownMenuItem>
+            );
+          })}
+          <DropdownMenuSeparator />
+          <DropdownMenuItem>
+            <SignOutLink />
+          </DropdownMenuItem>
+        </SignedIn>
       </DropdownMenuContent>
     </DropdownMenu>
   );
--- components/navbar/UserIcon.tsx
+++ components/navbar/UserIcon.tsx
@@ -1,4 +1,15 @@
+import { FaRegCircleUser } from 'react-icons/fa6';
+import { SignedOut, SignedIn } from '@clerk/nextjs';
 const UserIcon = () => {
-  return <div>UserIcon</div>;
+  return (
+    <>
+      <SignedOut>
+        <FaRegCircleUser className='w-10 h-10  rounded-full ' />
+      </SignedOut>
+      <SignedIn>
+        <FaRegCircleUser className='w-10 h-10  rounded-full text-primary' />
+      </SignedIn>
+    </>
+  );
 };
 export default UserIcon;
